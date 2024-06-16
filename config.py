a4ef1d74c6b1af0f92a4b22ac657771c93dbc78d
jokertroll <108746561+jokertroll@users.noreply.github.com>
2024-06-16 19:39:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "<b> 𝙃𝙚𝙡𝙡𝙤 ¯\_( ͡° ͜ʖ ͡°)_/¯ \n\n »»————>🄼🄰🄺<————««\n\n𝙏𝙧𝙮 --> /join</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
